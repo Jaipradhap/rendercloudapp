26432eb110f233249e4150c620365972a0fa1aff
Jaipradhap <jaipradhap.ma@gmail.com>
2022-08-02 00:56:32 +0400

    v2

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // images: {
-  //   loader: "akamai",
-  //   path: "",
-  // },
-  // assetPrefix: './'
+  images: {
+    loader: "akamai",
+    path: "",
+  },
+  assetPrefix: './'
   // basePath: "/",
   // assetPrefix: "/nextjs-pages",
 }
